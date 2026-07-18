61ba492a37d7c4118ef5479fead6b6cb97c28d2a
PavankumarRavipudi <pavankumarravipudi1608@gmail.com>
2026-07-18 12:38:22 +0530

    Add files via upload

---
+++ JDBCApp.java
@@ -0,0 +1,54 @@
+import java.util.*;
+import java.sql.*;
+public class JDBCApp
+{
+ public static void main(String args[])
+ {
+  String url="jdbc:mysql://localhost:3306/testdb?";
+  String user="testuser";
+  String password="testpass";
+  try
+  {
+   Class.forName("com.mysql.cj.jdbc.Driver");
+   Connection con=DriverManager.getConnection(url,user,password);
+   Statement st=con.createStatement();
+   String fquery="CREATE TABLE IF NOT EXISTS Student("+"Rollno INT PRIMARY KEY,"+"Name VARCHAR(50),"+"Address VARCHAR(100))";
+   st.executeUpdate(fquery);
+   System.out.println("Table created Successfully.");
+   st.executeUpdate("INSERT INTO Student VALUES(6,'rav','yedrabad')");
+   st.executeUpdate("INSERT INTO Student values(7,'sit','hennai')");
+   st.executeUpdate("INSERT INTO Student values(8,'iran','angalore')");
+   System.out.println("Initial records inserted.");
+   System.out.println("\nInitial Records:");
+   displayRecords(st);
+   st.executeUpdate("INSERT INTO Student values(9,'eena','une')");
+   st.executeUpdate("INSERT INTO Student values(10,'ram','goa')");
+   System.out.println("\nTwo new records inserted>");
+   st.executeUpdate("Update Student set Address='delhi' where Rollno=2");
+   System.out.println("one record updated");
+   st.executeUpdate("Delete from Student where Rollno=3");
+   System.out.println("one record deleted");
+   System.out.println("\n Final Records:");
+   displayRecords(st);
+   con.close();
+  }
+  catch(Exception e)
+  {
+   e.printStackTrace();
+   }
+  }
+  public static void displayRecords(Statement st) throws SQLException
+  {
+   ResultSet rs=st.executeQuery("SELECT * from Student");
+   System.out.println("Rollno\tName\tAddress");
+   while(rs.next())
+   {
+    int roll=rs.getInt("Rollno");
+    String name=rs.getString("name");
+    String address=rs.getString("Address");
+    System.out.println(roll+"\t"+name+"\t"+address);
+   } 
+  }
+}
+
+  
